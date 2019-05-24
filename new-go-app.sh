0eebd4ca89219fdb19ad71f978333aa2038d3abf
jay vyas <jay@apache.org>
2019-05-24 10:48:02 -0700

    Update new-go-app.sh

--- new-go-app.sh
+++ new-go-app.sh
@@ -1,5 +1,5 @@
 source other.sh
-
+NGA=/usr/local/bin/new-go-app/
 #### CHANGE THESE AS NEEDED AFTER CLONING, or keep them as defaults.  Either way should work.
 
 function getinput {
@@ -15,19 +15,20 @@ function getinput {
 
 function install_new_go_app {
         # Install new-go-app !
-        if [[ ! -d ~/.new-go-app ]] ; then
-                NEW_GO_APP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-                mv $NEW_GO_APP ~/.new-go-app/
+        if [[ ! -d /usr/local/bin/new-go-app/ ]] ; then
+                CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+                mkdir $NGA
         fi
+        echo "Updating /usr/local/bin/new-go-app !"
+        cp -r $CURR_DIR/* $NGA
 }
 
 function setup_app {
         WORKSPACE=$HOME/go/src/github.com/$YOUR_USERNAME/$PROJECT
         mkdir -p $WORKSPACE ;
-        cp -r ~/.new-go-app/my-app/* $WORKSPACE
+        cp -r $NGA/my-app/* $WORKSPACE
         chmod 755 $WORKSPACE/run.sh
 }
-
 
 function yay {
         echo "All done ! Now run ... "
