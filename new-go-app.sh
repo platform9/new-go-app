153bca8c6d845f0c38698540dcd09eb4b04eaeb2
jay vyas <jay@apache.org>
2019-05-24 10:38:03 -0700

    Update new-go-app.sh

--- new-go-app.sh
+++ new-go-app.sh
@@ -2,28 +2,40 @@ source other.sh
 
 #### CHANGE THESE AS NEEDED AFTER CLONING, or keep them as defaults.  Either way should work.
 
-echo "Enter your git or github username, anything will do:"
-read YOUR_USERNAME
-echo "Enter the name of this app, again, anything will do:"
-read PROJECT
+function getinput {
+        echo "Enter your git or github username, anything will do:"
+        read YOUR_USERNAME
 
-echo "Creating project $PROJECT under $YOUR_USERNAME, ok ?"
-read x
+        echo "Enter the name of this app, again, anything will do:"
+        read PROJECT
 
-# Install new-go-app !
-if [[ ! -d ~/.new-go-app ]] ; then
-        NEW_GO_APP=`pwd`
-        mv $NEW_GO_APP ~/.new-go-app/
-fi
+        echo "Creating project $PROJECT under $YOUR_USERNAME, ok ?"
+        read x
+}
 
-#### Any edits you need to make after here == a bug that i need to fix :)
+function install_new_go_app {
+        # Install new-go-app !
+        if [[ ! -d ~/.new-go-app ]] ; then
+                NEW_GO_APP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+                mv $NEW_GO_APP ~/.new-go-app/
+        fi
+}
 
-WORKSPACE=$HOME/go/src/github.com/$YOUR_USERNAME/$PROJECT
-mkdir -p $WORKSPACE ;
+function setup_app {
+        WORKSPACE=$HOME/go/src/github.com/$YOUR_USERNAME/$PROJECT
+        mkdir -p $WORKSPACE ;
+        cp -r ~/.new-go-app/my-app/* $WORKSPACE
+        chmod 755 $WORKSPACE/run.sh
+}
 
-echo "Changed directory to $WORKSPACE, created."
-cp -r ~/.new-go-app/my-app/* $WORKSPACE
 
-echo "All done ! Now run ... "
-echo "cd $WORKSPACE ; go run main.go"
-echo "And do something useful !!!"
+function yay {
+        echo "All done ! Now run ... "
+        echo "cd $WORKSPACE ; ./run.sh"
+        echo "And do something useful !!!"
+}
+
+getinput
+install_new_go_app
+setup_app
+yay
